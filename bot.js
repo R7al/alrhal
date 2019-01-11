ab71f663af8062177b4e9147e56135f8f2f8b3ba
R7al <46551749+R7al@users.noreply.github.com>
2019-01-10 23:13:08 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,19 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`SMART-BOT STARTED`);
+        console.log(`---------------`);
+      console.log(`ON ${client.guilds.size} Servers `);
+    console.log(`---------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame(`!X RUN...❥`,"http://twitch.tv/y04zgamer")
+   client.user.setStatus("dnd")
+});
+
 client.on('message', msg => {
   if (msg.content === 'hi') {
     msg.reply('Pong!');
